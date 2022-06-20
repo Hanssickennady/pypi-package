2e34d736ea17bdc3b8f8e3444126d7f189a941cf
Hanssic Kennady <hanssickennady@gmail.com>
2022-06-20 13:26:35 +0530

    initial commit

---
+++ pypack/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'guesser_game',
+    packages = ['guesser_game'],
+    version = 'v0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'initial release',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/Hanssickennady/pypi-package',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
